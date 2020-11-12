fb1fea6d95c46da7bd3028c535bf753af03751e0
Armando Fortes <afortes127@gmail.com>
2020-11-12 23:25:50 +0000

    feat: Add bootstrap and navbar

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/app.css';
